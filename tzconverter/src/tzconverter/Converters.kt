9570725705da21290ec51b0c273e4227a303fb45
jingapore <ngjingsong@gmail.com>
2025-11-13 21:07:36 +0800

    add cache for recordTransformer.

--- tzconverter/src/tzconverter/Converters.kt
+++ tzconverter/src/tzconverter/Converters.kt
@@ -1,12 +1,18 @@
 package tzconverter
 
+import java.util.concurrent.ConcurrentHashMap
 import org.apache.kafka.connect.connector.ConnectRecord
+import org.apache.kafka.connect.data.Schema
 
-fun <R : ConnectRecord<R>> valueTzConverter(cfg: Config): (R) -> R =
+internal fun <R : ConnectRecord<R>> valueTzConverter(cfg: Config): (R) -> R =
     recordTransformer(cfg, Lens.Value)
 
-fun <R : ConnectRecord<R>> keyTzConverter(cfg: Config): (R) -> R = recordTransformer(cfg, Lens.Key)
+internal fun <R : ConnectRecord<R>> keyTzConverter(cfg: Config): (R) -> R = recordTransformer(cfg, Lens.Key)
 
 private fun <R : ConnectRecord<R>> recordTransformer(cfg: Config, lens: Lens): (R) -> R {
+    val schemaCache = ConcurrentHashMap<Schema, Schema>()
+    return {record ->
+        lens.createNewRecord(record, converted.first, converted.second)
+    }
 
 }
