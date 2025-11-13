3f6a1aa5af81a84ecdde0994f2f9da35b2dbd9e2
jingapore <ngjingsong@gmail.com>
2025-11-14 02:03:01 +0800

    add skeleton for convertone.

--- tzconverter/src/tzconverter/Converters.kt
+++ tzconverter/src/tzconverter/Converters.kt
@@ -2,6 +2,7 @@ package tzconverter
 
 import java.util.concurrent.ConcurrentHashMap
 import org.apache.kafka.connect.connector.ConnectRecord
+import org.apache.kafka.connect.data.Date
 import org.apache.kafka.connect.data.Schema
 
 internal fun <R : ConnectRecord<R>> valueTzConverter(cfg: Config): (R) -> R =
@@ -15,6 +16,13 @@ private fun <R : ConnectRecord<R>> recordTransformer(cfg: Config, lens: Lens): (
     // and converting the schema accordingly. with this cache, we can skip such checks.
     val schemaCache = ConcurrentHashMap<Schema, Schema>()
     return { record ->
+        val schema = lens.getSchema(record)
+        val converted: Pair<Schema?, Any?> =
+            if (schema == null) {
+                null to null
+            } else {
+                null to null
+            }
         lens.createNewRecord(record, converted.first, converted.second)
     }
 }
@@ -25,24 +33,25 @@ private sealed interface Lens {
     fun <R : ConnectRecord<R>> createNewRecord(r: R, schema: Schema?, value: Any?): R
 
     data object Key : Lens {
-        override fun <R : ConnectRecord<R>> getValue(r: R): Any? = { TODO("asd") }
-        override fun <R : ConnectRecord<R>> getSchema(r: R): Schema? {
-            TODO("Not yet implemented")
-        }
-
+        override fun <R : ConnectRecord<R>> getValue(r: R): Any? = r.key()
+        override fun <R : ConnectRecord<R>> getSchema(r: R): Schema? = r.keySchema()
         override fun <R : ConnectRecord<R>> createNewRecord(r: R, schema: Schema?, value: Any?): R {
             TODO("Not yet implemented")
         }
     }
 
     data object Value : Lens {
-        override fun <R : ConnectRecord<R>> getValue(r: R): Any? = { TODO("asd") }
-        override fun <R : ConnectRecord<R>> getSchema(r: R): Schema? {
-            TODO("Not yet implemented")
-        }
-
+        override fun <R : ConnectRecord<R>> getValue(r: R): Any? = r.value()
+        override fun <R : ConnectRecord<R>> getSchema(r: R): Schema? = r.valueSchema()
         override fun <R : ConnectRecord<R>> createNewRecord(r: R, schema: Schema?, value: Any?): R {
             TODO("Not yet implemented")
         }
+    }
+}
+
+private fun convertOne(sourceValue: Date, cfg: Config): Any? {
+    when (cfg.targetType) {
+        TimestampTargetType.STRING -> TODO()
+        TimestampTargetType.DATE -> TODO()
     }
 }
